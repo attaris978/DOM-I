d0095000c9719d829e15f7719c4f34308cb59f2c
unknown <ryancare@protonmail.com>
2022-03-07 14:09:11 -0500

    Finished picture-perfect replication of the sample page via Javascript voodoo.

--- src/index.js
+++ src/index.js
@@ -38,5 +38,30 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+Array.from(document.querySelectorAll("nav a")).forEach((val,ind) => {
+  val.class = "italic";
+  let navItems = Object.values(siteContent.nav);
+  val.innerText = navItems[ind];
+} );
+document.querySelector("#logo-img").src = "../mocks/img/logo.png";
+document.querySelector("h1").innerText = Object.values(siteContent.cta)[0];
+document.querySelector(".cta-text button").innerText=Object.values(siteContent.cta)[1];
+document.querySelector("#cta-img").src = "../mocks/img/cta.png";
+document.querySelectorAll(".text-content h4").forEach( (val,ind) => {
+  let navArray = Object.values(siteContent["main-content"]).filter( (val,ind) => ind % 2 === 0);
+  val.innerText = navArray[ind];
+} );
+document.querySelectorAll(".text-content p").forEach( (val,ind) => {
+  let contentArray = Object.values(siteContent["main-content"]).filter( (val,ind) => ind % 2 === 1);
+  val.innerText = contentArray[ind];
+} );
+document.querySelector("#middle-img").src = "../mocks/img/accent.png";
+document.querySelector(".contact h4").innerText = siteContent.contact["contact-h4"];
+Array.from(document.querySelectorAll(".contact p")).forEach( (val,ind) => {
+  let contactArray = Object.values(siteContent.contact);
+  val.innerText = contactArray[ind + 1];
+} );
+document.querySelector("footer a").class = "bold";
+document.querySelector("footer a").innerText = "Copyright Great Idea! 2021";
 
 console.log('project wired!')
